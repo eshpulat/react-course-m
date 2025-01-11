241a8ae1bba12153d53884e8d5770d759c1af4af
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-01-11 11:52:33 -0500

    Add Ternaries Instead of if/else Statements

--- 02-js-review/script.js
+++ 02-js-review/script.js
@@ -170,5 +170,9 @@ updateBook;
 
 const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${
     publicationDate.split("-")[0]
-}`;
+}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted as a movie`;
 summary;
+
+const pagesRange = pages > 1000 ? "over a thousend" : "less than 1000";
+pagesRange;
+console.log(`The book has ${pagesRange} pages`);
