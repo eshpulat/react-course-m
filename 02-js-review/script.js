9c36c0ae67feb1f1a30160c885a36d57f37af505
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-01-14 18:48:54 -0500

    Add Asynchronous JavaScript: Async/Await and finish section-4

--- 02-js-review/script.js
+++ 02-js-review/script.js
@@ -273,8 +273,18 @@ function getBook(id) {
 // );
 // booksAfterUpdate;
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
 
-console.log("jonas");
+// console.log("jonas");
+
+async function getTodos() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await res.json();
+    console.log(data);
+}
+
+getTodos();
+
+console.log("Jonas");
