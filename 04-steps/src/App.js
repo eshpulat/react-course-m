078e4062a21b327d56600eb29e45dbb844525923
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-01-19 13:16:40 -0500

    Adding Another Piece of State

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -8,7 +8,8 @@ const messages = [
 
 export default function App() {
     const [step, setStep] = useState(1);
-    const [test, setTest] = useState({ name: "Jonas" });
+    const [isOpen, setIsOpen] = useState(true);
+    // const [test, setTest] = useState({ name: "Jonas" });
 
     function handledPrivious() {
         if (step > 1) setStep(step - 1);
@@ -19,35 +20,47 @@ export default function App() {
 
         // BAD PRACTICE
         //test.name = "Fred"
-
-        setTest({ name: "Fred" });
+        // setTest({ name: "Fred" });
     }
     return (
-        <div className="steps">
-            <div className="numbers">
-                <div className={step >= 1 ? "active" : ""}>1</div>
-                <div className={step >= 2 ? "active" : ""}>2</div>
-                <div className={step >= 3 ? "active" : ""}>3</div>
-            </div>
+        <>
+            <button className="close" onClick={() => setIsOpen(!isOpen)}>
+                &times;
+            </button>
+            {isOpen && (
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={step >= 1 ? "active" : ""}>1</div>
+                        <div className={step >= 2 ? "active" : ""}>2</div>
+                        <div className={step >= 3 ? "active" : ""}>3</div>
+                    </div>
 
-            <p className="message">
-                Step {step} : {messages[step - 1]}
-                {test.name}
-            </p>
-            <div className="buttons">
-                <button
-                    style={{ backgroundColor: "#7950f2", color: "#fff" }}
-                    onClick={handledPrivious}
-                >
-                    Previous
-                </button>
-                <button
-                    style={{ backgroundColor: "#7950f2", color: "#fff" }}
-                    onClick={handledNext}
-                >
-                    Next
-                </button>
-            </div>
-        </div>
+                    <p className="message">
+                        Step {step} : {messages[step - 1]}
+                        {/* {test.name} */}
+                    </p>
+                    <div className="buttons">
+                        <button
+                            style={{
+                                backgroundColor: "#7950f2",
+                                color: "#fff"
+                            }}
+                            onClick={handledPrivious}
+                        >
+                            Previous
+                        </button>
+                        <button
+                            style={{
+                                backgroundColor: "#7950f2",
+                                color: "#fff"
+                            }}
+                            onClick={handledNext}
+                        >
+                            Next
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
