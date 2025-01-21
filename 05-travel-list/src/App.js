c05c11f58dd15fbab8451d074cffde100f87b6da
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-01-21 13:48:18 -0500

    Deleting an Item: More Child-to-Parent Communication!

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -13,11 +13,15 @@ export default function App() {
         setItems((items) => [...items, item]);
     }
 
+    function handledDeleteItem(id) {
+        setItems((items) => items.filter((item) => item.id !== id));
+    }
+
     return (
         <div className="app">
             <Logo />
             <Form onAddItems={handledAddItems} />
-            <PackingList items={items} />
+            <PackingList items={items} onDeleteItem={handledDeleteItem} />
             <Stats />
         </div>
     );
@@ -73,25 +77,29 @@ function Form({ onAddItems }) {
     );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
     return (
         <div className="list">
             <ul>
                 {items.map((item) => (
-                    <Item item={item} key={item.id} />
+                    <Item
+                        item={item}
+                        onDeleteItem={onDeleteItem}
+                        key={item.id}
+                    />
                 ))}
             </ul>
         </div>
     );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
     return (
         <li>
             <span style={item.packed ? { textDecoration: "line-through" } : {}}>
                 {item.quantity} {item.description}
             </span>
-            <button>❌</button>
+            <button onClick={() => onDeleteItem(item.id)}>❌</button>
         </li>
     );
 }
