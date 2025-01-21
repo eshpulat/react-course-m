d7d6c1ec17a932565841cd12d604dafcc7d98326
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-01-21 13:32:00 -0500

    Add Thinking About State and Lifting State Up

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+    const [items, setItems] = useState([]);
+
+    function handledAddItems(item) {
+        setItems((items) => [...items, item]);
+    }
+
     return (
         <div className="app">
             <Logo />
-            <Form />
-            <PackingList />
+            <Form onAddItems={handledAddItems} />
+            <PackingList items={items} />
             <Stats />
         </div>
     );
@@ -21,7 +27,7 @@ function Logo() {
     return <h1>🏝️ Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
     const [description, setDescription] = useState("");
     const [quantity, setQuantity] = useState(1);
 
@@ -37,6 +43,8 @@ function Form() {
             id: Date.now()
         };
         console.log(newItem);
+
+        onAddItems(newItem);
 
         setDescription("");
         setQuantity(1);
@@ -65,11 +73,11 @@ function Form() {
     );
 }
 
-function PackingList() {
+function PackingList({ items }) {
     return (
         <div className="list">
             <ul>
-                {initialItems.map((item) => (
+                {items.map((item) => (
                     <Item item={item} key={item.id} />
                 ))}
             </ul>
