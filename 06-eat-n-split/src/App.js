885962d060dde49193ccfb0d6afac277be9f503a
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-02-03 11:05:22 -0500

    Using the Key Prop to Fix Our Eat-'N-Split App

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -78,6 +78,7 @@ export default function App() {
                 <FormSplitBill
                     selectFriend={selectFriend}
                     onSplitBill={handleSplitBill}
+                    key={selectFriend.id}
                 />
             )}
         </div>
