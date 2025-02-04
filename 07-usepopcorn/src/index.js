395f43aca532b1124b275e16e3b37c855f0aba05
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-02-04 11:49:17 -0500

    Add useEffect to the Rescue

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import StarRating from "./starRating";
+// import StarRating from "./starRating";
 
 // function Test() {
 //     const [movieRating, setMovieRating] = useState(0);
