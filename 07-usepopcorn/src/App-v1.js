9b4bfec7e50dedf4b426a14320a66a16ca3fcdb9
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-02-04 11:42:28 -0500

    How NOT to Fetch Data in React

--- 07-usepopcorn/src/App-v1.js
+++ 07-usepopcorn/src/App-v1.js
@@ -45,15 +45,9 @@ const tempWatchedData = [
 const average = (arr) =>
     arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "6ef49feb";
-
 export default function App() {
-    const [movies, setMovies] = useState([]);
-    const [watched, setWatched] = useState([]);
-
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-        .then((res) => res.json())
-        .then((data) => console.log(data.Search));
+    const [movies, setMovies] = useState(tempMovieData);
+    const [watched, setWatched] = useState(tempWatchedData);
 
     return (
         <>
--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,35 +1,35 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
 import StarRating from "./starRating";
 
-function Test() {
-    const [movieRating, setMovieRating] = useState(0);
-    return (
-        <div>
-            <StarRating
-                color="blue"
-                maxRating={10}
-                onSetRating={setMovieRating}
-            />
-            <p>This movie was Rated {movieRating} stars</p>
-        </div>
-    );
-}
+// function Test() {
+//     const [movieRating, setMovieRating] = useState(0);
+//     return (
+//         <div>
+//             <StarRating
+//                 color="blue"
+//                 maxRating={10}
+//                 onSetRating={setMovieRating}
+//             />
+//             <p>This movie was Rated {movieRating} stars</p>
+//         </div>
+//     );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        {/* <App /> */}
-        <StarRating
+        <App />
+        {/* <StarRating
             maxRating={"iyiugkj"}
             messages={["Okay", "Zor", "Gozal", "Gap yoq", "Sarvar"]}
         />
         <StarRating maxRating={10} />
-        <StarRating maxRating={10} defaultRating={3} />
+        <StarRating maxRating={10} defaultRating={3} /> */}
 
-        <Test />
+        {/* <Test /> */}
     </React.StrictMode>
 );
