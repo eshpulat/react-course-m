f11ec5638633acb35f42294c6f07f192ad1d69fa
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-02-06 12:51:29 -0500

    Using an async Function

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -50,11 +50,19 @@ const KEY = "6ef49feb";
 export default function App() {
     const [movies, setMovies] = useState([]);
     const [watched, setWatched] = useState([]);
+    const query = "intersteller";
 
     useEffect(function () {
-        fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-            .then((res) => res.json())
-            .then((data) => setMovies(data.Search));
+        async function fetchMovies() {
+            const res = await fetch(
+                `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            );
+
+            const data = await res.json();
+            setMovies(data.Search);
+            console.log(data.Search);
+        }
+        fetchMovies();
     }, []);
 
     return (
