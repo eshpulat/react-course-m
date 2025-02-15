ab0427e571b0f6add072f4469489bfa0d3c49b36
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-02-15 12:27:37 -0500

    Starting a New Quiz

--- 10-react-quize/src/App.js
+++ 10-react-quize/src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const intialState = {
     questions: [],
@@ -17,6 +18,9 @@ function reducer(state, action) {
             return { ...state, questions: action.payload, status: "ready" };
         case "dataFailed":
             return { ...state, status: "error" };
+
+        case "start":
+            return { ...state, status: "active" };
 
         default:
             throw new Error("Action unknown");
@@ -41,8 +45,12 @@ export default function App() {
                 {status === "Loading" && <Loader />}
                 {status === "error" && <Error />}
                 {status === "ready" && (
-                    <StartScreen numQuestions={numQuestions} />
+                    <StartScreen
+                        numQuestions={numQuestions}
+                        dispatch={dispatch}
+                    />
                 )}
+                {status === "active" && <Question />}
             </Main>
         </div>
     );
