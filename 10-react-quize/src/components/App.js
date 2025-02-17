1e8a48901ce0e34a34f4d27ee57d4093088eb8a8
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-02-17 12:29:06 -0500

    Finishing a Quiz

--- 10-react-quize/src/components/App.js
+++ 10-react-quize/src/components/App.js
@@ -7,6 +7,7 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const intialState = {
     questions: [],
@@ -14,7 +15,8 @@ const intialState = {
     status: "loading",
     index: 0,
     answer: null,
-    points: 0
+    points: 0,
+    highscore: 0
 };
 
 function reducer(state, action) {
@@ -42,16 +44,24 @@ function reducer(state, action) {
         case "nextQuestion":
             return { ...state, index: state.index + 1, answer: null };
 
+        case "finish":
+            return {
+                ...state,
+                status: "finished",
+                highscore:
+                    state.points > state.highscore
+                        ? state.points
+                        : state.highscore
+            };
+
         default:
             throw new Error("Action unknown");
     }
 }
 
 export default function App() {
-    const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-        reducer,
-        intialState
-    );
+    const [{ questions, status, index, answer, points, highscore }, dispatch] =
+        useReducer(reducer, intialState);
 
     const numQuestions = questions.length;
     const maxPossiblePoints = questions.reduce(
@@ -91,8 +101,20 @@ export default function App() {
                             dispatch={dispatch}
                             answer={answer}
                         />
-                        <NextButton dispatch={dispatch} answer={answer} />
+                        <NextButton
+                            dispatch={dispatch}
+                            answer={answer}
+                            numQuestions={numQuestions}
+                            index={index}
+                        />
                     </>
+                )}
+                {status === "finished" && (
+                    <FinishScreen
+                        maxPossiblePoints={maxPossiblePoints}
+                        points={points}
+                        highscore={highscore}
+                    />
                 )}
             </Main>
         </div>
