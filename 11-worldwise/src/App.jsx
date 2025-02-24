f0231043ef58e0624e5b16305fe6c2b9a8991e16
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-02-23 20:02:57 -0500

    Back to WorldWise: Creating a CitiesContext

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -6,65 +6,35 @@ import PageNotFound from "./pages/PageNotFound";
 import Applayout from "./pages/Applayout";
 import Login from "./pages/Login";
 import CityList from "./components/Citylist";
-import { useState } from "react";
-import { useEffect } from "react";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-
-const BASE_URL = "http://localhost:9000";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 function App() {
-    const [cities, setCities] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(function () {
-        async function fetchCities() {
-            try {
-                setIsLoading(true);
-                const res = await fetch(`${BASE_URL}/cities`);
-                const data = await res.json();
-                setCities(data);
-            } catch {
-                alert("There was an error loading data...");
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        fetchCities();
-    }, []);
-
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Homepage />}></Route>
-                <Route path="product" element={<Product />}></Route>
-                <Route path="pricing" element={<Pricing />}></Route>
-                <Route path="/login" element={<Login />}></Route>
-                <Route path="app" element={<Applayout />}>
-                    <Route index element={<Navigate replace to={cities} />} />
-                    <Route
-                        path="cities"
-                        element={
-                            <CityList cities={cities} isLoading={isLoading} />
-                        }
-                    />
+        <CitiesProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Homepage />} />
+                    <Route path="product" element={<Product />} />
+                    <Route path="pricing" element={<Pricing />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="app" element={<Applayout />}>
+                        <Route
+                            index
+                            element={<Navigate replace to="cities" />}
+                        />
+                        <Route path="cities" element={<CityList />} />
 
-                    <Route path="cities/:id" element={<City />} />
-                    <Route
-                        path="countries"
-                        element={
-                            <CountryList
-                                cities={cities}
-                                isLoading={isLoading}
-                            />
-                        }
-                    />
-                    <Route path="form" element={<Form />} />
-                </Route>
-                <Route path="*" element={<PageNotFound />}></Route>
-            </Routes>
-        </BrowserRouter>
+                        <Route path="cities/:id" element={<City />} />
+                        <Route path="countries" element={<CountryList />} />
+                        <Route path="form" element={<Form />} />
+                    </Route>
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </CitiesProvider>
     );
 }
 
