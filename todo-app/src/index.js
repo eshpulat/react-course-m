b2ce9ee7013d8017c682abdf355eccd413037571
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-03-04 22:31:17 -0500

    challange

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+    const [items, setItems] = useState([]);
+
+    function handleAddItems(item) {
+        setItems((items) => [...items, item]);
+    }
+
+    return (
+        <div>
+            <h1>To Do List</h1>
+            <TodoForm onAddItams={handleAddItems} />
+            <PackingList items={items} />
+        </div>
+    );
+}
+
+function TodoForm({ onAddItams }) {
+    const [description, setDescription] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const newItem = { description };
+        console.log(newItem);
+        onAddItams(newItem);
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="add-to-do-list"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                // key={description}
+            />
+            <button>Add</button>
+        </form>
+    );
+}
+
+// function PackingList({ items }) {
+//     return <li>{items}</li>;
+// }
+
+function PackingList({ items }) {
+    return (
+        <ul>
+            {items.map((item, index) => (
+                <li key={index}>{item.description}</li>
+            ))}
+        </ul>
+    );
+}
