084a1846697ad68fae887f39ee5a2356ef41281a
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-03-10 15:26:43 -0400

    Add Action Creators

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -35,11 +35,34 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// console.log(store.getState());
+
+// store.dispatch({
+//     type: "account/requestLoan",
+//     payload: { amount: 1000, purpose: "Buy a car" }
+// });
+// console.log(store.getState());
+
+function deposit(amount) {
+    return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+    return { type: "account/withdraw", payLoan: amount };
+}
+function requestLoan(amount, purpose) {
+    return {
+        type: "account/requestLoan",
+        payload: { amount, purpose }
+    };
+}
+function payLoan() {
+    return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
 console.log(store.getState());
 
-store.dispatch({
-    type: "account/requestLoan",
-    payload: { amount: 1000, purpose: "Buy a car" }
-});
-console.log(store.getState());
+store.dispatch(requestLoan(1000, "Buy a cheap car"));
+store.dispatch(payLoan());
