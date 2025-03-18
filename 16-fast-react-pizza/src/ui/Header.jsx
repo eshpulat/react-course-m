f80a94f3ccccf363df9ff84f7e11efc44e928fa8
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-03-17 20:35:49 -0400

    Building the App Layout

---
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <header>
+            <Link to="/">Fast react pizza Co.</Link>
+
+            <p>Muhammad</p>
+        </header>
+    );
+}
+
+export default Header;
