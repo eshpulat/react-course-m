f243e926034f1eddd02532ce6d3c089153a59484
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-03-20 21:39:13 -0400

    Add Fetching Orders

--- 16-fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
     return (
         <header>
             <Link to="/">Fast react pizza Co.</Link>
+
+            <SearchOrder />
 
             <p>Muhammad</p>
         </header>
