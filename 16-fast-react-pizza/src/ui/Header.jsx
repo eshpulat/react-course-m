f082d01d5abe51be2becee17965ee1d5ffdb79b6
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-03-21 20:01:09 -0400

    Add Styling Buttons: Element States and Transitions

--- 16-fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between bg-yellow-500 uppercase sm:px-6 border-b border-stone-200 px-4 py-3">
+    <header className="flex items-center justify-between bg-yellow-400 uppercase sm:px-6 border-b border-stone-200 px-4 py-3">
       <Link to="/" className="tracking-widest">
         Fast react pizza Co.
       </Link>
