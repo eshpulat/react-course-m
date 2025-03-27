e9811256be82d999e32a3e71d1e4fa09f5c334cb
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-03-26 21:28:57 -0400

    Modeling the User State With Redux Toolkit

--- 16-fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,12 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="text-sm hidden font-semibold md:block">Muhammad</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+  return (
+    <div className="text-sm hidden font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
