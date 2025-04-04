d6a85ae106b126869dd3c29a38b2186404709a60
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-04-04 12:20:05 -0400

    Setting Up Pages and Routes

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -1,52 +1,34 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStayles from "./styles/globalStyle";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.main`
-    /* background-color: orangered; */
-    padding: 20px;
-`;
 
 function App() {
     return (
         <>
             <GlobalStayles />
-            <StyledApp>
-                <Row type="vertical">
-                    <Row type="horizontal">
-                        <Heading as="h1">the Wild oasis</Heading>
-                        <div>
-                            <Heading as="h2">check in and out</Heading>
-                            <Button onClick={() => alert("check in")}>
-                                chek in
-                            </Button>
-                            <Button
-                                variation="secondary"
-                                size="small"
-                                onClick={() => alert("check out")}
-                            >
-                                chek out
-                            </Button>
-                        </div>
-                    </Row>
-                    <Row type="vertical">
-                        <Heading as="h3">Form</Heading>
-                        <form>
-                            <Input
-                                type="number"
-                                placeholder="Number the guest"
-                            ></Input>
-                            <Input
-                                type="number"
-                                placeholder="Number the guest"
-                            ></Input>
-                        </form>
-                    </Row>
-                </Row>
-            </StyledApp>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        index
+                        element={<Navigate replace to="dashboard" />}
+                    />
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="bookings" element={<Bookings />} />
+                    <Route path="cabins" element={<Cabins />} />
+                    <Route path="users" element={<Users />} />
+                    <Route path="settings" element={<Settings />} />
+                    <Route path="account" element={<Account />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
