c98627e79700885ea14a0ba303993eea039758c0
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-04-14 17:28:05 -0400

    Building a Modal Window Using a React Portal

--- 17-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ 17-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -10,7 +10,7 @@ import FormRow from "../../ui/FormRow";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
     const { createCabin, isCreating } = useCreateCabin();
     const { editCabin, isEditing } = useEditCabin();
     const isWorking = isCreating || isEditing;
@@ -36,6 +36,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
                 {
                     onSuccess: (data) => {
                         reset();
+                        onCloseModal?.();
                     }
                 }
             );
@@ -46,6 +47,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
             {
                 onSuccess: (data) => {
                     reset();
+                    onCloseModal?.();
                 }
             }
         );
@@ -56,7 +58,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
     }
 
     return (
-        <Form onSubmit={handleSubmit(onSubmit, onError)}>
+        <Form
+            onSubmit={handleSubmit(onSubmit, onError)}
+            type={onCloseModal ? "modal" : "regular"}
+        >
             <FormRow label="Cabin name" error={errors?.name?.message}>
                 <Input
                     type="text"
@@ -147,7 +152,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
             <FormRow>
                 {/* type is an HTML attribute! */}
-                <Button variation="secondary" type="reset">
+                <Button
+                    variation="secondary"
+                    type="reset"
+                    onClick={() => onCloseModal?.()}
+                >
                     Cancel
                 </Button>
                 <Button disabled={isWorking}>
